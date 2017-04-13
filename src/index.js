928f075a01a99214295ec009a9c553ffcb02b8af
Aaron <pratt.aaron.m@gmail.com>
2017-04-13 10:58:53 +0300

    first heroku commit with GraphQL API endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import Routes from './routes'
 
 ReactDOM.render(
-  <div>
-    <h1>Hello world!</h1>
-  </div>,
+  <Router
+  	history={browserHistory} // allows nice, pretty url and browser back/forward button
+  	routes={Routes}
+  />,
   document.getElementById('root')
 )
