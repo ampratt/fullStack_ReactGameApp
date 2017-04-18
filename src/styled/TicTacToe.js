4a732fd49587a1a100936fec5e92ce217326bacb
Aaron <pratt.aaron.m@gmail.com>
2017-04-18 21:02:02 +0300

    game functionality working with AI making random move

--- src/styled/TicTacToe.js
+++ src/styled/TicTacToe.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Layer, Line } from 'react-konva'
+import {Layer, Line, Text} from 'react-konva'
 
-export const Board = ({unitSquare, size, rows}) => {
+export const Board = ({unit, size, rows}) => {
 	let grid = []
 	let stroke = 'grey'
 	let strokeWidth = 10
 
-	//compose lines
-	for (let i=1; i < rows; i++){
-		let position = unitSquare * i
-		//vertical line
+	// compose lines
+	for (let i = 1; i < rows; i++) {
+		let position = unit * i
+		// vertical line
 		grid.push(
 			<Line
 				points={[position, 0, position, size]}
@@ -18,7 +18,7 @@ export const Board = ({unitSquare, size, rows}) => {
 				key={i+'v'}
 			/>
 		)
-		//horizontal line
+		// horizontal lines
 		grid.push(
 			<Line
 				points={[0, position, size, position]}
@@ -28,10 +28,56 @@ export const Board = ({unitSquare, size, rows}) => {
 			/>
 		)
 	}
-
 	return (
 		<Layer>
 			{grid}
+		</Layer>
+	)
+}
+
+export const Squares = ({
+	unit,
+	coordinates,
+	gameState,
+	win,
+	gameOver,
+	yourTurn,
+	ownMark,
+	move
+}) => {
+	let squares = coordinates.map( (position, index) => {
+			let makeMove = move
+			let mark = gameState[index]
+			let fill = 'black'
+			if (win && win.includes(index)) {
+				fill = 'lightgreen'
+			}
+			if (gameOver || !yourTurn || mark) {
+				makeMove = () => console.log('nope!')
+			}
+			return (
+				<Text
+					key={index}
+					index={index}
+					x={position[0]}
+					y={position[1]}
+					fontSize={unit}
+					width={unit}
+					text={mark}
+					fill={fill}
+					fontFamily={'Helvetica'}
+					align={'center'}
+					onClick={(event)=>{
+						let index = event.target.index
+						makeMove(index, ownMark)
+					}}
+				/>
+			)
+	})
+
+	return (
+		<Layer>
+			{squares}
 		</Layer>
 	)
 }
